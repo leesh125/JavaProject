c5d9d46da865314c868a4c1b5c657963791ffce2
leesh125 <chelsea12345@naver.com>
2021-08-04 17:43:12 +0900

    Day 09 Assignment Clear~!

--- project2/src/com/kh/exam2/NoAccessClass.java
+++
@@ -1,6 +0,0 @@
-package com.kh.exam2;
-
-class NoAccessClass {
-	public int attribute = 200;
-	public NoAccessClass() {};
-}
--- project2/src/com/kh/exam2/OtherClass.java
+++
@@ -1,6 +0,0 @@
-package com.kh.exam2;
-
-public class OtherClass {
-	public int attribute = 100;
-	public OtherClass() {}
-}
