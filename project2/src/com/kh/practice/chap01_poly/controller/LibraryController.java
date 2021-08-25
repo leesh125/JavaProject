e00c0d21b240bcfa9a355e4154550e2a3842e34f
leesh125 <chelsea12345@naver.com>
2021-08-25 15:27:51 +0900

    Day 22 Clear~! (Object Array to ArrayList)

--- project2/src/com/kh/practice/chap01_poly/controller/LibraryController.java
+++ project2/src/com/kh/practice/chap01_poly/controller/LibraryController.java
@@ -1,5 +1,7 @@
 package com.kh.practice.chap01_poly.controller;
 
+import java.util.ArrayList;
+
 import com.kh.practice.chap01_poly.model.vo.AniBook;
 import com.kh.practice.chap01_poly.model.vo.Book;
 import com.kh.practice.chap01_poly.model.vo.CookBook;
@@ -7,15 +9,19 @@ import com.kh.practice.chap01_poly.model.vo.Member;
 
 public class LibraryController {
 	private Member mem = null;
-	private Book[] bList = new Book[5];
+	//private Book[] bList = new Book[5];
+	private ArrayList<Book> bList = new ArrayList<Book>();
 	{
-		bList[0] = new CookBook("백종원의 집밥", "백종원", "tvN", true);
-		bList[1] = new AniBook("한번 더 해요", "미티", "원모어", 19);
-		bList[2] = new AniBook("루피의 원피스", "루피", "japan", 12);
-		bList[3] = new CookBook("이혜정의 얼마나 맛있게요", "이혜정", "문학", false);
-		bList[4] = new CookBook("최현석 날 따라해봐", "최현석", "소금책", true);
+		bList.add(new CookBook("백종원의 집밥", "백종원", "tvN", true));
+		bList.add(new AniBook("한번 더 해요", "미티", "원모어", 19));
+		bList.add(new AniBook("루피의 원피스", "루피", "japan", 12));
+		bList.add(new CookBook("이혜정의 얼마나 맛있게요", "이혜정", "문학", false));
+		bList.add(new CookBook("최현석 날 따라해봐", "최현석", "소금책", true));
+
 	}
 
+	
+	
 	public void insertMember(Member mem) {
 		this.mem = mem;
 	}
@@ -24,7 +30,7 @@ public class LibraryController {
 		return mem;
 	}
 	
-	public Book[] selectAll(){
+	public ArrayList<Book> selectAll(){
 		return bList;
 	}
 	
@@ -52,12 +58,13 @@ public class LibraryController {
 	public int rentBook(int index) {
 		int result = 0;
 		int i = 0;
-		if(bList[index] instanceof AniBook) {
-			if(((AniBook) bList[index]).getAccessAge() > mem.getAge()) {
+		
+		if(bList.get(index) instanceof AniBook) {
+			if(((AniBook) bList.get(index)).getAccessAge() > mem.getAge()) {
 				result = 1;
 			}
-		}else if(bList[index] instanceof CookBook) {
-			if(((CookBook) bList[index]).isCoupon() == true) {
+		}else if(bList.get(index) instanceof CookBook) {
+			if(((CookBook) bList.get(index)).isCoupon() == true) {
 				result = 2;
 				mem.setCouponCount(++i);
 			}
@@ -73,13 +80,20 @@ public class LibraryController {
 		this.mem = mem;
 	}
 
-	public Book[] getbList() {
+	public ArrayList<Book> getbList() {
 		return bList;
 	}
 
-	public void setbList(Book[] bList) {
+	public void setbList(ArrayList<Book> bList) {
+		
 		this.bList = bList;
 	}
+	
+	public void addBookList(Book b) {
+		bList.add(b);
+	}
+	
+	
 	
 	
 }
--- project2/src/com/kh/practice/chap01_poly/view/LibraryMenu.java
+++ project2/src/com/kh/practice/chap01_poly/view/LibraryMenu.java
@@ -1,5 +1,6 @@
 package com.kh.practice.chap01_poly.view;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.kh.practice.chap01_poly.controller.LibraryController;
@@ -30,6 +31,7 @@ public class LibraryMenu {
 			System.out.println("2. 도서 전체 조회");
 			System.out.println("3. 도서 검색");
 			System.out.println("4. 도서 대여하기");
+			System.out.println("5. 새 도서 추가하기");
 			System.out.println("9. 프로그램 종료하기");
 			System.out.print("메뉴 번호 : ");
 			int menu = sc.nextInt(); sc.nextLine();
@@ -47,6 +49,9 @@ public class LibraryMenu {
 			case 4:
 				this.rentBook();
 				break;
+			case 5:
+				this.addBookList();
+				break;
 			default:
 				System.out.println("번호를 다시 입력하세요.");
 			}
@@ -60,14 +65,17 @@ public class LibraryMenu {
 
 
 	public void selectAll() {
-		Book[] bList = lc.selectAll();
+		ArrayList<Book> bList = lc.selectAll();
 		int i = 0;
+//		for(Book b: bList) {
+//			if(b instanceof AniBook) {
+//				System.out.println(i++ + "번 도서 : " +b.toString());
+//			}else if(b instanceof CookBook) {
+//				System.out.println(i++ + "번 도서 : " +b.toString());		
+//			}
+//		}
 		for(Book b: bList) {
-			if(b instanceof AniBook) {
-				System.out.println(i++ + "번 도서 : " +b.toString());
-			}else if(b instanceof CookBook) {
-				System.out.println(i++ + "번 도서 : " +b.toString());		
-			}
+			System.out.println(i++ + "번 도서 : " + b);
 		}
 	}
 	
@@ -102,6 +110,50 @@ public class LibraryMenu {
 		}else if(result == 2) {
 			System.out.println("성공적으로 대여되었습니다. 요리학원 쿠폰이 발급되었으니 "
 					+ "마이페이지에서 확인하세요");
+		}
+	}
+	
+	public void addBookList() {
+		while(true) {
+			System.out.print("추가할 책 종류 입력(요리책 or 만화책) : ");
+			String bookKind = sc.nextLine();
+			
+			if(bookKind.equals("요리책")) {
+				System.out.print("추가할 책 이름 : ");
+				String bName = sc.nextLine();
+				
+				System.out.print("추가할 책의 작가 : ");
+				String bKind = sc.nextLine();
+	
+				System.out.print("추가할 책 출판사 : ");
+				String bPublish = sc.nextLine();
+	
+				System.out.print("추가할 책의 쿠폰 발급여부 : ");
+				Boolean isCoupon = sc.nextBoolean();
+	
+				lc.addBookList(new CookBook(bName, bKind, bPublish, isCoupon));
+				System.out.println(lc.getbList().get(lc.getbList().size()-1) + "-> 책이 추가 됨");
+				break;
+			}else if(bookKind.equals("만화책")) {
+				System.out.print("추가할 책 이름 : ");
+				String bName = sc.nextLine();
+				
+				System.out.print("추가할 책 작가 : ");
+				String bKind = sc.nextLine();
+	
+				System.out.print("추가할 책 출판사 : ");
+				String bPublish = sc.nextLine();
+	
+				System.out.print("추가할 책의 연령제한 : ");
+				int accessAge = sc.nextInt();  sc.nextLine();
+	
+				lc.addBookList(new AniBook(bName, bKind, bPublish, accessAge));
+				System.out.println(lc.getbList().get(lc.getbList().size()-1) + "-> 책이 추가 됨");
+				break;
+			}else {
+				System.out.println("요리책 or 만화책 중 입력!!");
+				continue;
+			}
 		}
 	}
 }
