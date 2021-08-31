9ffac9871aae7a98431e367b385946443b7c3e68
leesh125 <chelsea12345@naver.com>
2021-08-31 16:35:07 +0900

    Day 25 Assignment Clear~! (SQL-Univ Edu System)

--- project3/chap02/sample3.sql
+++ project3/chap02/sample3.sql
@@ -104,4 +104,168 @@ SELECT * FROM USER_CONSTRAINTS WHERE table_name = 'TEACHER_T';
 
 
 
+-- 학과 테이블
+DROP TABLE tb_department;
+CREATE TABLE tb_department(
+    department_no VARCHAR2(10),
+    department_name VARCHAR2(20),
+    category VARCHAR2(20),
+    open_yn CHAR(1),
+    capacity NUMBER
+);
+ALTER TABLE tb_department ADD CONSTRAINT TB_DEPARTMENT_NO_PK PRIMARY KEY(department_no);
+ALTER TABLE tb_department MODIFY department_name CONSTRAINT TB_DEPARTMENT_NAME_NN NOT NULL;
+ALTER TABLE tb_department ADD CONSTRAINT TB_DEPARTMENT_CATEGORY CHECK(category IN ('y', 'n'));
 
+COMMENT ON COLUMN tb_department.department_no IS '학과 번호';
+COMMENT ON COLUMN tb_department.department_name IS '학과 이름';
+COMMENT ON COLUMN tb_department.category IS '계열';
+COMMENT ON COLUMN tb_department.open_yn IS '개설 여부';
+COMMENT ON COLUMN tb_department.capacity IS '정원';
+
+SELECT * FROM ALL_ALL_TABLES WHERE table_name = 'TB_DEPARTMENT';
+SELECT * FROM ALL_TAB_COLUMNS WHERE table_name = 'TB_DEPARTMENT';
+SELECT * FROM ALL_COL_COMMENTS WHERE table_name = 'TB_DEPARTMENT';
+SELECT * FROM USER_CONSTRAINTS WHERE table_name = 'TB_DEPARTMENT';
+
+
+
+-- 학생 테이블
+DROP TABLE tb_student;
+CREATE TABLE tb_student(
+    student_no VARCHAR2(10),
+    department_no VARCHAR2(10),
+    student_name VARCHAR2(30),
+    student_ssn VARCHAR2(14),
+    student_address VARCHAR2(100),
+    entrance_date DATE,
+    absence_yn CHAR(1),
+    coach_professor_no VARCHAR2(10)
+);
+ALTER TABLE tb_student ADD CONSTRAINT TB_STUDENT_NO_PK PRIMARY KEY(student_no);
+ALTER TABLE tb_student ADD CONSTRAINT TB_STUDENT_DEPARTMENT_NO_FK FOREIGN KEY(department_no) REFERENCES tb_department(department_no);
+ALTER TABLE tb_student MODIFY student_name CONSTRAINT TB_STUDENT_NAME_NN NOT NULL;
+ALTER TABLE tb_student ADD CONSTRAINT TB_STUDENT_ABSENCE_YN CHECK(absence_yn IN ('y', 'n'));
+ALTER TABLE tb_student ADD CONSTRAINT TB_STUDENT_COACH_PROFESSOR_NO_FK FOREIGN KEY(coach_professor_no) REFERENCES tb_professor(professor_no);
+
+COMMENT ON COLUMN tb_student.student_no IS '학생 번호';
+COMMENT ON COLUMN tb_student.department_no IS '학과 번호';
+COMMENT ON COLUMN tb_student.student_name IS '학생 이름';
+COMMENT ON COLUMN tb_student.student_ssn IS '학생 주민번호 ';
+COMMENT ON COLUMN tb_student.student_address IS '학생 주소';
+COMMENT ON COLUMN tb_student.entrance_date IS '입학 일자';
+COMMENT ON COLUMN tb_student.absence_yn IS '휴학 여부';
+COMMENT ON COLUMN tb_student.coach_professor_no IS '지도 교수 번호';
+
+SELECT * FROM ALL_ALL_TABLES WHERE table_name = 'TB_STUDENT';
+SELECT * FROM ALL_TAB_COLUMNS WHERE table_name = 'TB_STUDENT';
+SELECT * FROM ALL_COL_COMMENTS WHERE table_name = 'TB_STUDENT';
+SELECT * FROM USER_CONSTRAINTS WHERE table_name = 'TB_STUDENT';
+
+
+
+
+-- 과목 테이블
+DROP TABLE tb_class;
+CREATE TABLE tb_class(
+    class_no VARCHAR2(10),
+    department_no VARCHAR2(10),
+    preattending_class_no VARCHAR2(10),
+    class_name VARCHAR2(30),
+    class_type VARCHAR2(10)
+);
+ALTER TABLE tb_class ADD CONSTRAINT TB_CLASS_NO_PK PRIMARY KEY(class_no);
+ALTER TABLE tb_class MODIFY DEPARTMENT_NO CONSTRAINT TB_CLASS_DEPARTMENT_NO_NN NOT NULL;
+ALTER TABLE tb_class ADD CONSTRAINT TB_CLASS_DEPARTMENT_NO_FK FOREIGN KEY(department_no) REFERENCES tb_department(department_no);
+ALTER TABLE tb_class MODIFY CLASS_NAME CONSTRAINT TB_CLASS_NAME_NN NOT NULL;
+
+COMMENT ON COLUMN tb_class.class_no IS '과목 번호';
+COMMENT ON COLUMN tb_class.department_no IS '학과 번호';
+COMMENT ON COLUMN tb_class.preattending_class_no IS '선수 과목 번호';
+COMMENT ON COLUMN tb_class.class_name IS '과목 이름';
+COMMENT ON COLUMN tb_class.class_type IS '과목 구분';
+
+SELECT * FROM ALL_ALL_TABLES WHERE table_name = 'TB_CLASS';
+SELECT * FROM ALL_TAB_COLUMNS WHERE table_name = 'TB_CLASS';
+SELECT * FROM ALL_COL_COMMENTS WHERE table_name = 'TB_CLASS';
+SELECT * FROM USER_CONSTRAINTS WHERE table_name = 'TB_CLASS';
+
+
+
+
+
+-- 과목 교수 테이블
+DROP TABLE tb_class_professor;
+CREATE TABLE tb_class_professor(
+    class_no VARCHAR2(10),
+    professor_no VARCHAR2(10)
+);
+
+ALTER TABLE tb_class_professor ADD CONSTRAINT TB_CLASS_PROFESSOR_PK PRIMARY KEY(class_no, professor_no);
+ALTER TABLE tb_class_professor ADD CONSTRAINT TB_CLASS_PROFESSOR_CLASS_NO_FK FOREIGN KEY(class_no) REFERENCES tb_class(class_no);
+ALTER TABLE tb_class_professor ADD CONSTRAINT TB_CLASS_PROFESSOR_PROFESSOR_NO_FK FOREIGN KEY(professor_no) REFERENCES tb_professor(professor_no);
+
+COMMENT ON COLUMN tb_class_professor.class_no IS '과목 번호';
+COMMENT ON COLUMN tb_class_professor.professor_no IS '교수 번호';
+
+SELECT * FROM ALL_ALL_TABLES WHERE table_name = 'TB_CLASS_PROFESSOR';
+SELECT * FROM ALL_TAB_COLUMNS WHERE table_name = 'TB_CLASS_PROFESSOR';
+SELECT * FROM ALL_COL_COMMENTS WHERE table_name = 'TB_CLASS_PROFESSOR';
+SELECT * FROM USER_CONSTRAINTS WHERE table_name = 'TB_CLASS_PROFESSOR';
+
+
+
+
+
+-- 교수 테이블
+DROP TABLE tb_professor;
+CREATE TABLE tb_professor(
+    professor_no VARCHAR2(10),
+    professor_name VARCHAR2(30),
+    professor_ssn VARCHAR2(14),
+    professor_address VARCHAR2(100),
+    department_no VARCHAR2(10)
+);
+ALTER TABLE tb_professor ADD CONSTRAINT TB_PROFESSOR_NO_PK PRIMARY KEY(professor_no);
+ALTER TABLE tb_professor MODIFY professor_name CONSTRAINT TB_PROFESSOR_NAME_NN NOT NULL;
+ALTER TABLE tb_professor ADD CONSTRAINT TB_PROFESSOR_DEPARTMENT_NO_FK FOREIGN KEY(department_no) REFERENCES tb_department(department_no);
+
+
+COMMENT ON COLUMN tb_professor.professor_no IS '교수 번호';
+COMMENT ON COLUMN tb_professor.professor_name IS '교수 이름';
+COMMENT ON COLUMN tb_professor.professor_ssn IS '교수 주민번호';
+COMMENT ON COLUMN tb_professor.professor_address IS '교수 주소';
+COMMENT ON COLUMN tb_professor.department_no IS '학과 번호';
+
+SELECT * FROM ALL_ALL_TABLES WHERE table_name = 'TB_PROFESSOR';
+SELECT * FROM ALL_TAB_COLUMNS WHERE table_name = 'TB_PROFESSOR';
+SELECT * FROM ALL_COL_COMMENTS WHERE table_name = 'TB_PROFESSOR';
+SELECT * FROM USER_CONSTRAINTS WHERE table_name = 'TB_PROFESSOR';
+
+
+
+
+-- 성적 테이블
+DROP TABLE tb_grade;
+CREATE TABLE tb_grade(
+    term_no VARCHAR2(10),
+    class_no VARCHAR2(30),
+    student_no VARCHAR2(14),
+    point NUMBER
+);
+ALTER TABLE tb_grade ADD CONSTRAINT TB_GRADE_TERM_NO_PK PRIMARY KEY(term_no);
+ALTER TABLE tb_grade ADD CONSTRAINT TB_GRADE_CLASS_NO_FK FOREIGN KEY(class_no) REFERENCES tb_class(class_no);
+ALTER TABLE tb_grade ADD CONSTRAINT TB_GRADE_STUDNET_NO_FK FOREIGN KEY(student_no) REFERENCES tb_student(student_no);
+ALTER TABLE tb_grade ADD CONSTRAINT TB_GRADE_POINT CHECK(point IN (3,2));
+
+COMMENT ON COLUMN tb_grade.term_no IS '학기 번호';
+COMMENT ON COLUMN tb_grade.class_no IS '과목 번호';
+COMMENT ON COLUMN tb_grade.student_no IS '학생 번호';
+COMMENT ON COLUMN tb_grade.point IS '학점';
+
+SELECT * FROM ALL_ALL_TABLES WHERE table_name = 'TB_GRADE';
+SELECT * FROM ALL_TAB_COLUMNS WHERE table_name = 'TB_GRADE';
+SELECT * FROM ALL_COL_COMMENTS WHERE table_name = 'TB_GRADE';
+SELECT * FROM USER_CONSTRAINTS WHERE table_name = 'TB_GRADE';
+
+SELECT * FROM ALL_ALL_TABLES WHERE table_name LIKE 'TB%';
