3f2d124fda56f0e80e572146c2c1e835246e57a7
leesh125 <chelsea12345@naver.com>
2021-10-22 15:27:14 +0900

    Day 56 Clear~! (Frontend -jQuery)

--- project4/static/js/ch09_1.js
+++ project4/static/js/ch09_1.js
@@ -1,29 +1,57 @@
-window.onload = function() {
+// window.onload = function() {
+//     initial();
+// }
+
+$(document).ready(function() {
     initial();
-}
+});
 
 function initial() {
-    var accordion = document.getElementsByClassName("accordion")[0];
-    var accordion_items = accordion.querySelectorAll(".accordion-item");
-    for(var item of accordion_items) {
-        var title = item.querySelector(".accordion-title");
-        title.addEventListener("click",
-            function(e) {
-                var src = e.target;
-                var isOpend = src.getAttribute("class").indexOf("active") == -1 ? false : true;
-                for(var other of accordion_items) {
-                    other.querySelector(".accordion-title").classList.remove("active");
-                    other.querySelector(".accordion-content").classList.remove("open");
-                }
+    var accordion = $(".accordion").eq(0);
+    var accordion_items = accordion.find(".accordion-item");
+    
+    accordion_items.each(
+        function(idx, item) {
+            var title = $(item).find(".accordion-title");
+            title.on("click",
+                function(e) {
+                    var src = $(e.target);
+                    var isOpend = src.is(".active");
+                    src.parent().parent().find(".accordion-title").removeClass("active");
+                    src.parent().parent().find(".accordion-content").removeClass("open");
 
-                if(isOpend) {
-                    src.classList.remove("active");
-                    src.nextElementSibling.classList.remove("open");
-                } else {
-                    src.classList.add("active");
-                    src.nextElementSibling.classList.add("open");
+                    if(isOpend) {
+                        src.removeClass("active");
+                        src.next().removeClass("open");
+                    } else {
+                        src.addClass("active");
+                        src.next().addClass("open");
+                    }
                 }
-            }
-        );
-    }
-} 
+            );
+        }
+    );
+
+    // var accordion = document.getElementsByClassName("accordion")[0];
+    // var accordion_items = accordion.querySelectorAll(".accordion-item");
+    // for(var item of accordion_items) {
+    //     var title = item.querySelector(".accordion-title");
+    //     title.addEventListener("click",
+    //         function(e) {
+    //             var src = e.target;
+    //             var isOpend = src.getAttribute("class").indexOf("active") == -1 ? false : true;
+    //             for(var other of accordion_items) {
+    //                 other.querySelector(".accordion-title").classList.remove("active");
+    //                 other.querySelector(".accordion-content").classList.remove("open");
+    //             }
+    //             if(isOpend) {
+    //                 src.classList.remove("active");
+    //                 src.nextElementSibling.classList.remove("open");
+    //             } else {
+    //                 src.classList.add("active");
+    //                 src.nextElementSibling.classList.add("open");
+    //             }
+    //         }
+    //     );
+    // }
+}
